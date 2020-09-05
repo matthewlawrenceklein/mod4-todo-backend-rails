a1a448bf80762ce18406ab7e3968fc2a117dded1
matthewlawrenceklein <mtt.l.kln@gmail.com>
2020-09-05 16:00:01 -0500

    handle post req, cors issue addressed

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -9,4 +9,15 @@ class TodosController < ApplicationController
         todo = Todo.all.find(params[:id])
         render json: todo 
     end
+
+    def create
+        todo = Todo.create(todo_params)
+        render json: todo
+    end 
+
+    private 
+
+    def todo_params
+        params.permit(:body, :color, :user_id)
+    end 
 end
